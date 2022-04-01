e0fd3f3e01beefc0c41e3bd49eb2b80ffae6e906
Niklas Strand <nikstra@users.noreply.github.com>
2022-04-01 20:36:01 +0200

    Change date for fake data.

--- DataBinding/Services/CalendarService.cs
+++ DataBinding/Services/CalendarService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using DataBinding.Interfaces;
 using DataBinding.Models;
-using DataBinding.Pages.Components.Calendar;
 
 namespace DataBinding.Services
 {
@@ -18,49 +17,49 @@ namespace DataBinding.Services
                 new CalendarEntry
                 {
                     Name = "Meeting 1",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 4, 10, 0, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 4, 10, 0, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 2",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 14, 13, 10, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 14, 13, 10, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 3",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 14, 10, 0, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 14, 10, 0, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 4",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 14, 11, 15, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 14, 11, 15, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 5",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 21, 11, 15, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 21, 11, 15, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 6",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 21, 11, 15, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 21, 11, 15, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 7",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 21, 11, 15, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 21, 11, 15, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
                 new CalendarEntry
                 {
                     Name = "Meeting 8",
-                    Start = new DateTimeOffset(new DateTime(2022, 3, 21, 11, 15, 0)),
+                    Start = new DateTimeOffset(new DateTime(2022, 4, 21, 11, 15, 0)),
                     Duration = TimeSpan.FromMinutes(45)
                 },
             }.AsEnumerable());
