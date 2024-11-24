8fec1c7e0874bdda858cba4fb3d8abf25e14cc22
Niklas Strand <nikstra@users.noreply.github.com>
2024-11-24 20:34:47 +0100

    Use HtmlAttributeBuilder NuGet package.

--- WebUi/Components/Calendar/Models/Day.cs
+++ WebUi/Components/Calendar/Models/Day.cs
@@ -14,8 +14,12 @@ namespace WebUi.Components.Calendar.Models
         public IEnumerable<PublicHolidayModel> Holidays { get; set; } = Enumerable.Empty<PublicHolidayModel>();
         public string[] GetHolidayLocalNames() =>
             Holidays.Where(h => !string.IsNullOrWhiteSpace(h.LocalName))
+                 // Same name can be used in different regions. But, should we filter that here?
+                .DistinctBy(h => h.LocalName, StringComparer.InvariantCultureIgnoreCase)
                 .Select(h => h.LocalName)
                 .ToArray();
-
+        // TODO: Optimize logic for properties. Rename properties paying attention to css class names.
+        public bool CalendarHolidayListIndicator => Holidays.Count() > 2;
+        public bool Holiday => Holidays.Any();
     }
 }
